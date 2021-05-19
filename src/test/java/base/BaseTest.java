92b3618ac07472b1e36716f304ae4ba94817c619
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2021-05-19 00:00:36 -0500

    fix error

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,26 +3,58 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+@Listeners(TestOrder.class)
 public abstract class BaseTest {
+
+    public static final String HUB_URL = "http://localhost:4444/wd/hub";
+
+    private static boolean remoteWebDriver = false;
+
+    static {
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(HUB_URL + "/status").openConnection();
+            try {
+                con.setRequestMethod("GET");
+                remoteWebDriver = con.getResponseCode() == HttpURLConnection.HTTP_OK;
+            } finally {
+                con.disconnect();
+            }
+        } catch (IOException ignore) {}
+
+        if (!remoteWebDriver) {
+            WebDriverManager.chromedriver().setup();
+        }
+    }
+
+    public static boolean isRemoteWebDriver() {
+        return remoteWebDriver;
+    }
 
     private WebDriver driver;
 
-    @BeforeClass
-    protected void beforeClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeMethod
     protected void beforeMethod() {
-        driver = new ChromeDriver();
+        if (isRemoteWebDriver()) {
+            try {
+                driver = new RemoteWebDriver(new URL(HUB_URL), new ChromeOptions());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
     }
 
     @AfterMethod
