e7f7434cbfa37f3d58dd4f4998d9b80f697e03f7
Sergey Kuznetsov <votkinsk@gmail.com>
2021-05-19 20:10:27 -0700

    Hotfix (#159)
    Replace junit → testng

--- src/test/java/OlesiaAbdullinaTest.java
+++ src/test/java/OlesiaAbdullinaTest.java
@@ -1,5 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
