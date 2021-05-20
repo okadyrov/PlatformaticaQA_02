4de854ad1a599b921951de0cf589d65561338535
macuser <mishinaolia95@gmail.com>
2021-05-20 12:46:15 -0700

    Refactoring: Removed throws and unnecessary methods.

--- src/test/java/OlgaMishinaTest.java
+++ src/test/java/OlgaMishinaTest.java
@@ -1,23 +1,14 @@
 import base.BaseTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import net.bytebuddy.implementation.bytecode.Throw;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.sql.SQLException;
-
 
 public class OlgaMishinaTest extends BaseTest {
 
   @Test
-  public void test() throws InterruptedException, SQLException {
-      getDriver().get("https://www.zippershipper.com/");
+  public void testOlgaMishina() {
+        getDriver().get("https://www.zippershipper.com/");
 
         WebElement input = getDriver().findElement(By.xpath("//input[@id='txtRedirectSearchBox']"));
         input.sendKeys("Dress Zippers\n");
@@ -32,4 +23,3 @@ public class OlgaMishinaTest extends BaseTest {
         Assert.assertTrue(image.isDisplayed());
       }
   }
-
