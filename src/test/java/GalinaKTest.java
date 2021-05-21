010b09ae4c6387df1105aa7fc3cf981a9b584e87
Galina <gkukleva734@gmail.com>
2021-05-21 11:03:41 -0600

    CI restarted

--- src/test/java/GalinaKTest.java
+++ src/test/java/GalinaKTest.java
@@ -24,7 +24,6 @@ public class GalinaKTest extends BaseTest {
             address = getDriver().findElement(By.xpath("//div [@id = 'parent-fieldname-text']/div/div/p[9]")).getText();
         }
 
-        Assert.assertEquals(address, "Location:\n" +
-                "6705 Fortuna NW\n" + "Albuquerque, NM 87121\n" + "Map");
+        Assert.assertEquals(address, "Location:\n" + "6705 Fortuna NW\n" + "Albuquerque, NM 87121\n" + "Map");
     }
 }
