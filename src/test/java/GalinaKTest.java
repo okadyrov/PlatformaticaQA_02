026dcc1f446a0cdc2b49812ea7eea10c2f7020fe
Galina <gkukleva734@gmail.com>
2021-05-21 08:57:22 -0600

    XPath corrected, logic added to check if text presents on the page

--- src/test/java/GalinaKTest.java
+++ src/test/java/GalinaKTest.java
@@ -1,6 +1,7 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,16 +10,21 @@ public class GalinaKTest extends BaseTest {
     @Test
     public void testGalinaK() {
 
+        WebDriverWait wait = new WebDriverWait(getDriver(),20);
+
         getDriver().get("https://www.cabq.gov/parksandrecreation/recreation/swimming/indoor-pools");
 
         getDriver().findElement(
                 By.xpath("//div[@id='parent-fieldname-text']/div/div/p/a[@data-val='e8f7542fb4e0de27b3ea4a0e73a708a5']")
         ).click();
 
-        WebElement address = getDriver().findElement(
-                By.xpath("//p[text() = '6705 Fortuna NW']"));
+        String address = "";
+        if (wait.until(ExpectedConditions.textToBePresentInElementLocated(
+                By.xpath("//div [@id = 'parent-fieldname-text']/div/div/p[9]/strong"),"Location:"))) {
+            address = getDriver().findElement(By.xpath("//div [@id = 'parent-fieldname-text']/div/div/p[9]")).getText();
+        }
 
-        Assert.assertEquals(address.getText(), "Location:\n" +
+        Assert.assertEquals(address, "Location:\n" +
                 "6705 Fortuna NW\n" + "Albuquerque, NM 87121\n" + "Map");
     }
 }
