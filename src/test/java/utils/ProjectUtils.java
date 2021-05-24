ef01d532e08410fd263ccb629fe33fe1af111923
Daria-Razzhigaeva <82791007+Daria-Razzhigaeva@users.noreply.github.com>
2021-05-24 15:23:38 -0700

    Fixed reset method. Last line was unstable. Sometimes Platformatica tests failed. (#203)

--- src/test/java/utils/ProjectUtils.java
+++ src/test/java/utils/ProjectUtils.java
@@ -68,8 +68,9 @@ public class ProjectUtils {
     }
 
     public static void reset(WebDriver driver) {
+        driver.findElement(By.id("navbarDropdownProfile")).click();
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("navbarDropdownProfile"))).click();
-        driver.findElement(By.xpath("//a[@href=\"index.php?action=reset\"]")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[contains(text(),'!!! Reset all for my user !!!')]"))).click();
     }
 }
