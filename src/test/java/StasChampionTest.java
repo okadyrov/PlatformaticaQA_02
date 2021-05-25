2949e8903929f2daf09f2ab0cca6bd8719f75465
Staschampion <82689880+Staschampion@users.noreply.github.com>
2021-05-25 09:21:28 -0700

    Stas champion test1 (#196)

--- src/test/java/StasChampionTest.java
+++ src/test/java/StasChampionTest.java
@@ -1,3 +1,4 @@
+import base.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,50 +7,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class StasChampionTest {
+public class StasChampionTest extends BaseTest {
 
-    private WebDriver driver;
-
-    @BeforeClass
-    public void before() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeMethod
-    public void beforeTest() {
-        driver = new ChromeDriver();
-    }
-
-    @Ignore
     @Test
     public void test1StasChampion() throws InterruptedException {
-        driver.get("https://google.com/");
+        getDriver().get("https://google.com/");
 
-        WebElement input = driver.findElement(By.className("gLFyf"));
+        WebElement input = getDriver().findElement(By.className("gLFyf"));
         input.sendKeys("123\n");
 
-        WebElement result = driver.findElement(
+        WebElement result = getDriver().findElement(
                 By.xpath("//*[@id=\"wp-tabs-container\"]/div[1]/div/div/div/div/div/div/div[2]/h3/a/h3"));
 
         Assert.assertEquals(result.getText(), "Jess Glynne - 123 [Official Live Video] - YouTube");
     }
 
-    @Ignore
     @Test
     public void test2StasChampion() throws InterruptedException {
-        driver.get("https://bukovel.com/en/");
+        getDriver().get("https://bukovel.com/en/");
 
-        Thread.sleep(5000);
-
-        WebElement readMoreButton = driver.findElement(
+        WebElement readMoreButton = getDriver().findElement(
                 By.xpath("//div[@id='scaleContainer']/div/div/a[contains(text(), 'Read more')]"));
         readMoreButton.click();
-        WebElement text = driver.findElement(By.xpath("//div[@class='inner-info-text ']"));
-        Assert.assertEquals(text.getText(), "WINTER SEASON CLOSING DATE SCHEDULED FOR MAY 10TH");
-    }
-
-    @AfterMethod
-    public void afterTest() {
-        driver.quit();
+        WebElement text = getDriver().findElement(By.xpath("//div[@class='inner-info-text ']"));
+        Assert.assertEquals(text.getText(), "«ARTEK-BUKOVEL»: CAMP BOOKING HAS STARTED!");
     }
 }
